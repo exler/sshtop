7d7327639fc5c9ed5907b3757c25420233da97fd
Kamil Marut <kamil.marut@pm.me>
2021-04-19 12:27:07 +0200

    Show ip addresses instead of network interfaces

--- sshtop/monitor.py
+++ sshtop/monitor.py
@@ -4,15 +4,17 @@ from time import sleep
 
 from sshtop import helpers
 
-def get_hostname(ssh_client):
-    stdout = ssh_client.exec_command('hostname')[1]
 
-    return stdout.read().decode('utf-8').rstrip('\n\r')
+def get_hostname(ssh_client):
+    stdout = ssh_client.exec_command("hostname")[1]
+
+    return stdout.read().decode("utf-8").rstrip("\n\r")
+
 
 def get_uptime(ssh_client):
-    stdout = ssh_client.exec_command('/usr/bin/cut -d. -f1 /proc/uptime')[1]
+    stdout = ssh_client.exec_command("/usr/bin/cut -d. -f1 /proc/uptime")[1]
 
-    up = int(stdout.read().decode('utf-8'))
+    up = int(stdout.read().decode("utf-8"))
     secs = up % 60
     mins = int((up / 60) % 60)
     hours = int((up / 3600) % 24)
@@ -22,16 +24,17 @@ def get_uptime(ssh_client):
 
 
 def get_loadavg(ssh_client):
-    stdout = ssh_client.exec_command('cat /proc/loadavg')[1]
+    stdout = ssh_client.exec_command("cat /proc/loadavg")[1]
 
-    loadavg = stdout.read().decode('utf-8').rstrip('\n\r').split()
+    loadavg = stdout.read().decode("utf-8").rstrip("\n\r").split()
 
     return f"{loadavg[0]}, {loadavg[1]}, {loadavg[2]}  (1, 5, 15 min)"
 
-def get_memory(ssh_client):
-    stdout = ssh_client.exec_command('cat /proc/meminfo')[1]
 
-    memory = stdout.read().decode('utf-8').split('\n')
+def get_memory(ssh_client):
+    stdout = ssh_client.exec_command("cat /proc/meminfo")[1]
+
+    memory = stdout.read().decode("utf-8").split("\n")
     memfree = int(memory[1].split()[1]) / 1024
     memtotal = int(memory[0].split()[1]) / 1024
     swapfree = int(memory[15].split()[1])
@@ -44,31 +47,20 @@ def get_memory(ssh_client):
         buffers: {buffers:.2f}MB
         cached: {cached:.2f}MB"""
 
-def get_filesystems(ssh_client):
-    stdout = ssh_client.exec_command('df -h')[1]
 
-    filesystems = stdout.read().decode('utf-8').split('\n')[1:-1]
+def get_filesystems(ssh_client):
+    stdout = ssh_client.exec_command("df -h")[1]
+
+    filesystems = stdout.read().decode("utf-8").split("\n")[1:-1]
 
     return filesystems
 
-def get_networks(ssh_client):
-    stdout = ssh_client.exec_command('ifconfig eth0')[1]
-    eth = stdout.read().decode('utf-8')
 
-    stdout = ssh_client.exec_command('ifconfig wifi0')[1]
-    wifi = stdout.read().decode('utf-8')
+def get_ip_addresses(ssh_client):
+    stdout = ssh_client.exec_command(f"hostname -I")[1]
+    ips = stdout.read().decode("utf-8").split()
 
-    if eth and wifi:
-        return f"""{helpers.format_network_interface('eth0', eth)}
-            {helpers.format_network_interface('wifi0', wifi)}
-        """
-    elif eth:
-        return helpers.format_network_interface('eth0', eth)
-    elif wifi:
-        return helpers.format_network_interface('wifi0', wifi)
-    else:
-        return "No Ethernet or WiFi interfaces found!"
-
+    return "\n\t".join(ips)
 
 
 def start_monitoring(ssh_client, interval):
@@ -79,7 +71,7 @@ def start_monitoring(ssh_client, interval):
         loadavg = get_loadavg(ssh_client)
         memory = get_memory(ssh_client)
         filesystems = get_filesystems(ssh_client)
-        networks = get_networks(ssh_client)
+        ip_addresses = get_ip_addresses(ssh_client)
 
         helpers.clear_screen()
 
@@ -98,7 +90,7 @@ def start_monitoring(ssh_client, interval):
             if not len(filesystem[2]) == 1:
                 helpers.print_body(f"\t{filesystem[5]}: {filesystem[2]} used out of {filesystem[1]}")
 
-        helpers.print_header(f"\nNetwork Interfaces:")
-        helpers.print_body(f"\t{networks}")
+        helpers.print_header(f"\nIP Addresses:")
+        helpers.print_body(f"\t{ip_addresses}")
 
         sleep(interval)
